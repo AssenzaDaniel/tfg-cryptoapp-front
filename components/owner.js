8b72106f6cbc41af8fa9148fa55840cbba48dc89
Daniel Assenza <assenzadaniel@gmail.com>
2023-04-21 23:18:56 +0200

    Creación de prueba de componente en JavaScript

---
+++ components/owner.js
@@ -0,0 +1,51 @@
+class Owner extends HTMLElement {
+
+    #name
+
+    constructor(name = '') {
+        super()
+
+        this.#name = name
+        this.addEventListener('click', this.#modifyName)
+    }
+
+    get name() {
+        return this.#name
+    }
+
+    set name(value) {
+
+        if (value === this.#name) return
+
+        this.#name = value
+        this.render()
+    }
+
+    #modifyName(event) {
+
+        const newName = window.prompt('Ingresa nuevo nombre')
+        this.name = newName || ''
+        event.stopPropagation()
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+
+    attributeChangedCallback(attribute, oldValue, newValue = '') {
+
+        if (oldValue !== newValue) this[attribute] = newValue
+    }
+
+    static get observedAttributes() { 
+        return ['name']; 
+    }
+
+    render() {
+        this.innerHTML = `
+        <h2>Bienvenido, ${this.#name}.</h2>
+        `
+    }
+}
+
+customElements.define('user-greeting', Owner)
