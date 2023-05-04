52ec0d997c245e2f97b155b566991fe2ee6387b2
Malcoby <malcoby17@gmail.com>
2023-05-04 02:51:08 +0200

    Created new js components (login, loginbtn)

---
+++ src/components/buttons/loginbtn.js
@@ -0,0 +1,77 @@
+class LoginButton extends HTMLElement {
+
+    #src = ''
+    #socialName = ''
+    #__img__ = null
+    #alreadyRendered = false
+    #bindedElement = null
+    #onEvent = null
+
+    constructor(iconSrc) {
+        super()
+
+        this.#src = iconSrc
+    }
+
+    get src() {
+        return this.#src
+    }
+
+    set src(value) {
+        this.#src = value
+        this.#__img__.src = value
+    }
+
+    set onClick(action) {
+
+        this.#onEvent = action
+        this.addEventListener('click', this.#onEvent)
+    }
+
+    /**
+     * @param { String } event Event to add listener
+     * @param { HTMLElement } element Element to bind action
+     * @param { Function } action Action to bind
+     */
+    bind(event, element, action) {
+        this.#bindedElement = element
+        this.#onEvent = action.bind(element)
+
+        this.addEventListener(event, this.#onEvent)
+    }
+
+    static get observedAttributes() {
+        return ['src'];
+    }
+
+    connectedCallback() {
+        this.#src = this.getAttribute('src')
+        this.#socialName = this.getAttribute('alt')
+
+        this.render()
+        this.#__img__ = this.querySelector('img')
+        this.#alreadyRendered = true
+    }
+
+    attributeChangedCallback(attribute, oldValue, newValue) {
+
+        if (this.#alreadyRendered && newValue !== oldValue) 
+            this[attribute] = newValue
+    }
+
+    render() {
+
+        this.innerHTML = `
+        <div class="row">
+            <button type="button" class="mybtn">
+                <img src="${ this.#src }" alt="${ this.#socialName }">
+                <a href="#">
+                    Signup using ${ this.#socialName }</a>
+            </button>
+        </div>
+        `
+    }
+}
+
+window.customElements.define('login-button', LoginButton)
+export default LoginButton
---
+++ src/components/login.js
@@ -0,0 +1,48 @@
+class Login extends HTMLElement {
+
+    #alreadyRendered = false
+    #bindedElement = null
+    #onEvent = null
+
+    constructor() {
+        super()
+    }
+
+    /**
+     * @param { String } event Event to add listener
+     * @param { HTMLElement } element Element to bind action
+     * @param { Function } action Action to bind
+     */
+    bind(event, element, action) {
+        this.#bindedElement = element
+        this.#onEvent = action.bind(element)
+
+        this.addEventListener(event, this.#onEvent)
+    }
+
+    connectedCallback() {
+
+        this.render()
+        this.#alreadyRendered = true
+    }
+
+    attributeChangedCallback(attribute, oldValue, newValue) {
+
+        if (this.#alreadyRendered && newValue !== oldValue) 
+            this[attribute] = newValue
+    }
+
+    render() {
+
+        this.innerHTML = `
+        <div class="container">
+            <login-button src="assets/google.svg" alt="Google"></login-button>
+            <login-button src="assets/github.svg" alt="GitHub"></login-button>
+            <login-button src="assets/facebook.svg" alt="Facebook"></login-button>
+        </div>
+        `
+    }
+}
+
+window.customElements.define('user-login', Login)
+export default Login
