abe135982e7325f9b769a5fbdb8f542eefc6fb2d
Daniel Assenza <assenzadaniel@gmail.com>
2023-06-03 17:09:10 +0200

    Small changes to prepare server deployment

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,9 @@
-import { subscribeSymbol, search, getSymbols } from '../services/index.js'
+import { search, getSymbols } from '../services/index.js'
 import SearchBar from './bars/searchbar.js'
 import Table from './tables/table.js'
 
 class Search extends HTMLElement {
 
-    #alreadyRendered = false
     #isActive = false
 
     #searchBar = null
@@ -36,55 +35,11 @@ class Search extends HTMLElement {
             document.body.style.overflow = 'scroll'
         } else {
             document.body.style.overflow = 'hidden'
-            window.scroll(0, 1)
             this.querySelector('search-bar').focus()
             this.setAttribute('active', '')
         }
 
         this.#isActive = !this.#isActive
-    }
-
-    #updateTable(symbols) {
-        symbols.forEach(symbol => {                    
-            const div = document.createElement('div')
-            div.className = 'symbol'
-            div.id = symbol.symbol
-
-            const changeType = symbol.priceChangePercent >= 0 
-                ? 'positive'
-                : 'negative'
-    
-            div.innerHTML = `
-            <div class="symbol-data">
-                ${ window.atob(symbol.icon) }
-                <span class="symbol">${ symbol.symbol }</span>
-            </div>
-            <div class="price">
-                <div>${ symbol.lastPrice }</div>
-                <div class="${ changeType }">${ symbol.priceChangePercent }%</div>
-            </div>
-            <menu-button src="search-fav.svg"></menu-button>
-            `
-
-            div.addEventListener('click', async () => {
-
-                const symbol = div.querySelector('span.symbol').textContent
-                const subscribe = subscribeSymbol('assenzadaniel@gmail.com', symbol)
-
-                subscribe.then(() => {
-
-                    div.getAttribute('selected') !== null
-                        ? div.removeAttribute('selected')
-                        : div.setAttribute('selected', '')
-                })
-            })
-
-            if (symbol.favorite) {
-                div.setAttribute('selected', '')
-            }
-
-            this.#searchTable.appendChild(div)
-        })
     }
 
     render() {
